0e4d340363dc1e48c42ddaab4c35b949828617e1
Kyle <kylemilner@me.com>
2016-02-12 23:32:58 +0000

    added guava and gson dependencies, / endpoint now accepts optional name query parameter. response is now JSON

--- src/main/java/com/kyrobot/howfar/Server.java
+++ src/main/java/com/kyrobot/howfar/Server.java
@@ -1,10 +1,12 @@
 package com.kyrobot.howfar;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
- 
-import static spark.Spark.*;
+import static spark.Spark.get;
+
+import java.util.Optional;
+
+import com.google.gson.Gson;
+
+import spark.ResponseTransformer;
 
 /**
  * Main App Entry point
@@ -12,8 +14,19 @@ import static spark.Spark.*;
  */
 public class Server 
 {
+	public static ResponseTransformer json = (ResponseTransformer) (o) -> new Gson().toJson(o);
+	
+	public static class WelcomeMessage {
+		public String greeting;
+	}
+	
     public static void main( String[] args )
     {
-        get("/", (req, res) -> "Hello, World!");
+        get("/", (req, res) -> {
+        	final Optional<String> maybeName = Optional.ofNullable(req.queryParams("name"));
+        	final WelcomeMessage hello = new WelcomeMessage();
+        	hello.greeting="Hello, " + maybeName.orElse("World");
+        	return hello;
+        }, json);
     }
 }

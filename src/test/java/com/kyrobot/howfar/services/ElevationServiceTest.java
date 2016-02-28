4c010ac388845e13948c279a6155564f675a2794
Kyle <kylemilner@me.com>
2016-02-28 12:39:14 +0000

    more tests, added testutils

--- src/test/java/com/kyrobot/howfar/services/ElevationServiceTest.java
+++ src/test/java/com/kyrobot/howfar/services/ElevationServiceTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.junit.After;
 import org.junit.Before;
@@ -28,6 +27,11 @@ import spark.Spark;
 public class ElevationServiceTest {
 	
 	private final static String API_ROOT = "http://0.0.0.0:4567/api/elevation/";
+	
+	private final long major_id = 0L;
+	private final String major_name = "Major Thing";
+	private final int major_height = 100;
+	
 	private final Gson gson = new Gson();
 	
 	@Mock DataAccessObject<HighTarget> mockDAO;
@@ -35,8 +39,8 @@ public class ElevationServiceTest {
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		final Stream<HighTarget> majors = newArrayList(new HighTarget(0L, "Tall Thing", 100)).stream();
-		when(mockDAO.getMajor()).thenReturn(majors);
+		final List<HighTarget> majors = newArrayList(new HighTarget(major_id, major_name, major_height));
+		when(mockDAO.getMajor()).thenReturn(majors.stream());
 		ElevationService service = new ElevationService(mockDAO);
 		service.defineRoutes();
 		Spark.awaitInitialization();
@@ -51,8 +55,8 @@ public class ElevationServiceTest {
 	public void testMetersMajors() throws Exception {
 		final String json = ServiceTestUtils.doGET(API_ROOT + "meters/100");
 		final ElevationResponse elevationResponse = gson.fromJson(json, ElevationResponse.class);
-		final List<ElevationMilestone> majorMilestones = Lists.newArrayList(elevationResponse.getMajorMilestones());
-		
+
+		final List<ElevationMilestone> majorMilestones = newArrayList(elevationResponse.getMajorMilestones());
 		assertTrue(majorMilestones.size() == 1);
 		
 		double completion = majorMilestones.get(0).completion;
@@ -60,11 +64,56 @@ public class ElevationServiceTest {
 		assertEquals(1.0, completion, 0);
 		
 		final HighTarget target = majorMilestones.get(0).milestone;
+		checkTarget(target, major_id, major_name, major_height);
+	}
+	
+	@Test
+	public void testFloorsMajors() throws Exception {
+		// A Floor is 3.048 meters -> 5 floors is 15.24m
+		// Target is 100 meters
+		// Expect completion -> 0.152 to 3 decimal places
 		
+		final String json = ServiceTestUtils.doGET(API_ROOT + "floors/5");
+		final ElevationResponse elevationResponse = gson.fromJson(json, ElevationResponse.class);
+		
+		final List<ElevationMilestone> majorMilestones = Lists.newArrayList(elevationResponse.getMajorMilestones());
+		assertTrue(majorMilestones.size() == 1);
+		
+		double completion = majorMilestones.get(0).completion;
+		assertNotNull(completion);
+		assertEquals(0.152, completion, 0);
+		
+		final HighTarget target = majorMilestones.get(0).milestone;
+		checkTarget(target, major_id, major_name, major_height);
+		
+	}
+	
+	@Test
+	public void testFeetMajors() throws Exception {
+		// A Foot is 0.3048 -> 8 feet is 2.4384m
+		// Target is 100 meters
+		// Expect completion -> 0.024 to 3 decimal places
+		
+		final String json = ServiceTestUtils.doGET(API_ROOT + "feet/8");
+		final ElevationResponse elevationResponse = gson.fromJson(json, ElevationResponse.class);
+		
+		final List<ElevationMilestone> majorMilestones = Lists.newArrayList(elevationResponse.getMajorMilestones());
+		assertTrue(majorMilestones.size() == 1);
+		
+		double completion = majorMilestones.get(0).completion;
+		assertNotNull(completion);
+		assertEquals(0.024, completion, 0);
+		
+		final HighTarget target = majorMilestones.get(0).milestone;
+		checkTarget(target, major_id, major_name, major_height);
+		
+	}
+	
+	private void checkTarget(HighTarget target, long expectedId, String expectedName, int expectedHeight) {
 		assertNotNull(target);
-		assertEquals("Tall Thing", target.getName());
-		assertEquals(100, target.getHeight());
-		assertEquals(0L, target.getId());
+		assertEquals(expectedName, target.getName());
+		assertEquals(expectedHeight, target.getHeight());
+		assertEquals(expectedId, target.getId());
 	}
 
 }
---
+++ src/test/java/com/kyrobot/howfar/utils/ServiceTestUtils.java
@@ -0,0 +1,16 @@
+package com.kyrobot.howfar.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class ServiceTestUtils {
+	
+	public static String doGET(String url) throws Exception {
+		final HttpGet get = new HttpGet(url);
+		final HttpEntity entity = HttpClients.createMinimal().execute(get).getEntity();
+		return EntityUtils.toString(entity);
+	}
+
+}
